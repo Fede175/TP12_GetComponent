8d9e2c7fb6a5b5fb73310ac357cb0b0a9c087da9
FEDERICO CASTELLANO <Militoplayer@gmail.com>
2026-05-05 14:25:58 -0300

    Initial commit

---
+++ Assets/Scripts/ColisisonGravedad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisisonGravedad : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Hubo Colision");
+        cubeData = col.gameObject.GetComponent<CubeScript>();
+        Debug.Log("Nombre del otro objeto: " + cubeData.objectName);
+    }
+}
